f28e6a87302b9c40be5d8679b7009eb705c6a680
shdab <130516923+shadatr@users.noreply.github.com>
2024-04-26 21:08:28 +0300

    wallet added

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -1,16 +1,14 @@
 import { FC, ReactNode, useMemo } from "react";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
+import {PhantomWalletAdapter,SolflareWalletAdapter} from "@solana/wallet-adapter-wallets";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import * as web3 from "@solana/web3.js";
 import * as walletAdapterWallets from "@solana/wallet-adapter-wallets";
-require("@solana/wallet-adapter-react-ui/styles.css");
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const endpoint = web3.clusterApiUrl("devnet");
-  const wallets = useMemo(() => [], []);
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], [new SolflareWalletAdapter()]);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
